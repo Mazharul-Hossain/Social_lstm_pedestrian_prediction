5acbd43588543bfbf08ecf4aef36426b169f6a01
Mazhar <2561458+m-h@users.noreply.github.com>
2019-11-24 00:29:25 -0600

    Error correction

--- Original project revised/lstm_new/social_visualize.py
+++ Original project revised/lstm_new/social_visualize.py
@@ -72,7 +72,7 @@ def plot_trajectories(true_trajs, pred_trajs, obs_length, name, save_location):
         pred_x = [(p[0] + 1) / 2 for p in pred_traj_ped]
         pred_y = [(p[1] + 1) / 2 for p in pred_traj_ped]
 
-        print(true_x, true_y, pred_x, pred_y)
+        # print(true_x, true_y, pred_x, pred_y)
 
         plt.plot(true_x, true_y, color='g', linestyle='solid', marker='o')
         plt.plot(pred_x, pred_y, color='b', linestyle='dashed', marker='x')
@@ -87,6 +87,97 @@ def plot_trajectories(true_trajs, pred_trajs, obs_length, name, save_location):
     plt.close()
 
 
+def video_trajectories(results_pkl, save_location=None):
+    '''
+    Function that plots the true trajectories and the
+    trajectories predicted by the model alongside
+    params:
+    true_trajs : numpy matrix with points of the true trajectories
+    pred_trajs : numpy matrix with points of the predicted trajectories
+    Both parameters are of shape traj_length x maxNumPeds x 3
+    obs_length : Length of observed trajectory
+    name: Name of the plot
+    '''
+    if save_location is None:
+        save_location = 'video/'
+
+    f = open(results_pkl, 'rb')
+    # f = open('save/3/social_results.pkl', 'rb')
+    # was f = open('save/social_results.pkl', 'rb')
+    results = pickle.load(f)
+
+    zeros = '000'
+    for i in range(len(results)):
+        digits = len(str(abs(int(i))))
+        name = 'sequence' + zeros[:-digits] + str(i)
+        true_trajs, pred_trajs, obs_length = results[i][0], results[i][1], results[i][2]
+
+        traj_length, maxNumPeds, _ = true_trajs.shape
+
+        # Initialize figure
+        plt.figure()
+
+        # Load the background
+        # im = plt.imread('plot/plot.png')
+        # implot = plt.imshow(im)
+        # width = im.shape[0]
+        # height = im.shape[1]
+        # width = 1
+        # height = 1
+
+        traj_data = {}
+        # For each frame/each point in all trajectories
+        for i in range(traj_length):
+            pred_pos = pred_trajs[i, :]
+            true_pos = true_trajs[i, :]
+
+            # For each pedestrian
+            for j in range(maxNumPeds):
+                if true_pos[j, 0] == 0:
+                    # Not a ped
+                    continue
+                elif pred_pos[j, 0] == 0:
+                    # Not a ped
+                    continue
+                else:
+                    # If he is a ped
+                    if true_pos[j, 1] > 1 or true_pos[j, 1] < 0:
+                        continue
+                    elif true_pos[j, 2] > 1 or true_pos[j, 2] < 0:
+                        continue
+
+                    if (j not in traj_data) and i < obs_length:
+                        traj_data[j] = [[], []]
+
+                    if j in traj_data:
+                        traj_data[j][0].append(true_pos[j, 1:3])
+                        traj_data[j][1].append(pred_pos[j, 1:3])
+
+        for j in traj_data:
+            # c = np.random.rand(3, 1)
+            true_traj_ped = traj_data[j][0]  # List of [x,y] elements
+            pred_traj_ped = traj_data[j][1]
+
+            true_x = [(p[0] + 1) / 2 for p in true_traj_ped]
+            true_y = [(p[1] + 1) / 2 for p in true_traj_ped]
+            pred_x = [(p[0] + 1) / 2 for p in pred_traj_ped]
+            pred_y = [(p[1] + 1) / 2 for p in pred_traj_ped]
+
+            # print(true_x, true_y, pred_x, pred_y)
+
+            plt.plot(true_x, true_y, color='g', linestyle='solid', marker='o')
+            plt.plot(pred_x, pred_y, color='b', linestyle='dashed', marker='x')
+
+        # plt.ylim((0, 1))
+        # plt.xlim((0, 1))
+        plt.title(name)
+        # plt.show()
+
+        plt.savefig(os.path.join(save_location, 'plot_' + name + '.png'))
+        plt.gcf().clear()
+        plt.close()
+
+
 def visualize(results_pkl, save_location=None):
     if save_location is None:
         save_location = 'plot/'
@@ -96,6 +187,8 @@ def visualize(results_pkl, save_location=None):
     # was f = open('save/social_results.pkl', 'rb')
     results = pickle.load(f)
 
+    zeros = '000'
     for i in range(len(results)):
-        name = 'sequence' + str(i)
+        digits = len(str(abs(int(i))))
+        name = 'sequence' + zeros[:-digits] + str(i)
         plot_trajectories(results[i][0], results[i][1], results[i][2], name, save_location)
